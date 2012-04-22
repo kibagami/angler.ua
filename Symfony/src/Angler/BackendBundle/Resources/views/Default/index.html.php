6747aa74bebc0fc6efe054fe23ebffc668e7ecde
Maksym Borysenko <borysenko.maksym@gmail.com>
2012-04-23 00:00:36 +0300

    Added general html layout for Backend bundle

--- Symfony/src/Angler/BackendBundle/Resources/views/Default/index.html.php
+++ Symfony/src/Angler/BackendBundle/Resources/views/Default/index.html.php
@@ -1,12 +1,1 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-	<meta charset="UTF-8">
-	<title>Hello, Maxim</title>
-</head>
-<body>
-<div id="page">
-	Hello, Maxim!!!
-</div>
-</body>
-</html>
+
---
+++ Symfony/src/Angler/BackendBundle/Resources/views/layout.html.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+<div id="page">
+
+</div>
+</body>
+</html>
